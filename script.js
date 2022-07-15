3ce5fd0b2fbadee3d906ce3297990497e4bf189c
OllyKu <oly.kuskova@gmail.com>
2022-07-15 22:44:54 +0300

    new feature timer function

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //score counter function
-let score = 0;
+// let score = 0;
+
+
+// timer function
+
+let timeleft = 10;
+let downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("timeDown").innerHTML = "TimeOut";
+  } else {
+    document.getElementById("timeDown").innerHTML = timeleft + " seconds";
+  }
+  timeleft -= 1;
+}, 1000);
